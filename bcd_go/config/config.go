6f264779be384ca0cfad76521b4c0411b05a3e97
baichangda <471267877@qq.com>
2023-04-30 22:27:41 +0800

    变更

--- bcd_go/config/config.go
+++ bcd_go/config/config.go
@@ -3,17 +3,27 @@ package config
 import (
 	"context"
 	"github.com/go-redis/redis/v8"
+	"os"
 	"time"
 )
 
-const redisAddress = "43.138.63.155:6379"
-const redisPwd = "bcd"
+var redisAddress = "cdbai.cn:6379"
+var redisPwd = "bcd5221527"
 
 var RedisClient *redis.Client
 var RedisCtx = context.Background()
 
 func InitRedis() {
+
 	if RedisClient == nil {
+		env1 := os.Getenv("REDIS_HOST")
+		env2 := os.Getenv("REDIS_PWD")
+		if env1 != "" {
+			redisAddress = env1
+		}
+		if env2 != "" {
+			redisPwd = env2
+		}
 		RedisClient = redis.NewClient(&redis.Options{
 			Addr:         redisAddress,
 			Password:     redisPwd,
