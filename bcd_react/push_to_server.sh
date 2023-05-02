f02b1040a2239aaee0733309d8662cb911336c5c
baichangda <471267877@qq.com>
2023-05-02 12:19:43 +0800

    变更

--- bcd_react/push_to_server.sh
+++ bcd_react/push_to_server.sh
@@ -1,1 +1,1 @@
-rsync -avz --exclude={.idea,.gradle,logs,*.log,temp} /Users/baichangda/bcd/webworkspace/bcd_react/build/* root@43.138.63.155:/app/software/nginx/html
+rsync -e 'ssh -p 60022' -avz --exclude={.idea,.gradle,logs,*.log,temp} /Users/baichangda/bcd/bcd/bcd_react/build/* root@cdbai.cn:/app/software/nginx-1.24.0/html
